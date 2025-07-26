0eb80c9dd1ac421361a6d32ad57441fd6e5186b4
bhanuprakash-6 <mail.bhanuprakash.7@gmail.com>
2025-07-26 22:08:24 +0530

    cloudinary and multer

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs";
+
+ // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Click 'View API Keys' above to copy your cloud name
+        api_key: process.env.CLOUDINARY_API_KEY, // Click 'View API Keys' above to copy your API key
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+    
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) {
+            return null;
+        }
+        //uploading the file to cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto" // Automatically detect the resource type (image, video, etc.)
+        })
+        console.log("File uploaded successfully to Cloudinary",response.url);
+        return response;
+    }catch(error){
+        fs.unlinkSync(localFilePath); // Delete the local file if upload fails
+        console.error("Error uploading file to Cloudinary:", error);
+        return null;
+    }
+}
+
+export { uploadOnCloudinary };
